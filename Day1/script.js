50c46dec06fd03a75f0511a95b7c072849ff801e
yewinnaingedem <yewinnaing11605@gmail.com>
2024-05-17 08:39:46 -0700

    feat : intro javascript alogrithmos

---
+++ Day1/script.js
@@ -0,0 +1,18 @@
+const mathTest = Math.floor(Math.random() * ( 3 - 1 + 1));
+// console.log(mathTest);
+
+const getH1 = document.querySelectorAll('h2').length;
+const arrayFrom = Array.from(getH1);
+// console.log(arrayFrom + "Array Like" + "\n" + getH1);
+
+const hello = [
+    "this" ,
+    "is" ,
+    "the" ,
+    "greeting" ,
+    "text" ,
+];
+// math function take 
+// a function as a parameter and that function how many parameter will accept 
+const greeting  = hello.map((index) => index).join(" ");
+console.log(greeting);
